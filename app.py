4be1359c9ddf3a6a3cd27090f3ee9e3cb8e996e0
buro.de.identidad.d@gmail.com <buro.de.identidad.d@gmail.com>
2020-03-07 01:56:38 -0600

    AJGD|add phones proyect

--- app.py
+++ app.py
@@ -1,35 +1,56 @@
 from flask import Flask, jsonify, request
 import phonenumbers
 from phonenumbers import geocoder
+import csv
+
+import codecs
 
 app = Flask(__name__)
 
-@app.route('/ping')
-def ping():
-    return jsonify({"Pong!":""})
+#curl --request GET \
+#  --url 'http://127.0.0.1:4000/phones/(213)%20416-0509'
+@app.route('/phones/<string:number>', methods = ['GET'])
+def addProductPost(number):
+    text = number
+    for match in phonenumbers.PhoneNumberMatcher(text, "US"):
+        numberFormated = (phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))    
+    z = phonenumbers.parse(numberFormated, None)
+    validNumber = phonenumbers.is_valid_number(z)
+    ch_number = phonenumbers.parse(numberFormated, "CH")
+    geo = geocoder.description_for_number(ch_number, "es")
+    #numbers, valid, location
+    return jsonify({"number":numberFormated,"valid":validNumber, "location":geo})
 
-from products import products
-@app.route('/products', methods = ['GET'])
-def getProducts():
-    return jsonify({"Products:":products})
+#curl --request POST \
+#  --url http://127.0.0.1:4000/phones/csv \
+#  --header 'content-type: multipart/form-data; boundary=---011000010111000001101001' \
+#  --form numbers=
+@app.route('/phones/csv', methods=['POST'])
+def handle_form():
+    print("Posted file: {}".format(request.files['numbers']))
+           
+    file = request.files['numbers']
+    read = csv.reader(codecs.iterdecode(file, 'utf-8'))
 
-    
-@app.route('/products/<string:name>', methods = ['GET'])
-def getProduct(name):
-    print(name)
-    product = [product for product in products if product['nombre']==name]
-    if(len(product)>0):
-        return jsonify(product[0])
-    return jsonify({"Products:":"producto no encontrado"})
+    my_list = []
+    for row in read:
+        #print(row[0])
+        for match in phonenumbers.PhoneNumberMatcher(row[0], "US"):
+            numberFormated = (phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))    
+            z = phonenumbers.parse(numberFormated, None)
+            validNumber = phonenumbers.is_valid_number(z)
+            ch_number = phonenumbers.parse(numberFormated, "CH")
+            geo = geocoder.description_for_number(ch_number, "es")
+            #numbers, valid, location
+            my_list.append(str(row[0]+", "+str(validNumber)+", "+ifnull(geo,"n/a")))
+            print(row[0], validNumber, geo)
 
+    return jsonify(results = my_list)
 
-@app.route('/products', methods = ['POST'])
-def addProductPost():
-    num = request.json['data']
-    x = phonenumbers.parse("2134160509", None)
-    geo = geocoder.description_for_number(x, "es")
-    print(geo)
-    return jsonify(geo)
+def ifnull(var, val):
+  if var is "":
+    return val
+  return var
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
--- products.py
+++
@@ -1,5 +0,0 @@
-products = [
-    {"nombre":"laptop", "price":800 , "cant":2},
-    {"nombre":"laptop1", "price":300 , "cant":1},
-    {"nombre":"laptop2", "price":500 , "cant":3},
-]
